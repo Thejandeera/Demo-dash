358a1278282faf25811e8112272795582b121a66
Sandeepana K M T <137618345+Thejandeera@users.noreply.github.com>
2026-07-06 17:50:06 +0530

    dev : created theme with blue

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,22 @@
 
 import React from "react";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { store } from "@/store";
+import theme from "@/theme/theme";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </Provider>
+  );
 }
