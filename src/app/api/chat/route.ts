c35564a3c136d62cc9ee1c983e17e885cbb17a42
Sandeepana K M T <137618345+Thejandeera@users.noreply.github.com>
2026-07-08 11:36:38 +0530

    dev : created api route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,57 @@
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  isStepCount,
+  createUIMessageStreamResponse,
+  toUIMessageStream,
+} from 'ai';
+import { z } from 'zod';
+import { google } from '@ai-sdk/google';
+
+export async function POST(req: Request) {
+ 
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: google('gemini-2.0-flash'),
+    messages: await convertToModelMessages(messages),
+    stopWhen: isStepCount(5),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: 'Convert a temperature in fahrenheit to celsius',
+        inputSchema: z.object({
+          temperature: z.number().describe('The temperature in fahrenheit to convert'),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
+    onStepEnd: ({ toolResults }) => {
+      console.log('Tool Step Completed:', toolResults);
+    },
+  });
+
+
+  return createUIMessageStreamResponse({
+    stream: toUIMessageStream({ stream: result.stream }),
+  });
+}
